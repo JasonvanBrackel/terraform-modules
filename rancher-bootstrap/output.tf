cd2764d5e012226caf8c9a7199e0b904e0d7b0e0
Jason L. van Brackel <jason@vanbrackel.net>
2018-05-01 13:13:02 -0500

    Add bootstrap script.

--- rancher-bootstrap/output.tf
+++ rancher-bootstrap/output.tf
@@ -1,48 +1,47 @@
-output "etcd-nodes" {
+output "etcd_nodes" {
   value = "${azurerm_public_ip.etcd_publicip.*.ip_address}"
 }
 
-output "controlplane-nodes" {
+output "controlplane_nodes" {
   value = "${azurerm_public_ip.controlplane_publicip.*.ip_address}"
 }
 
-
-output "worker-nodes" {
+output "worker_nodes" {
   value = "${azurerm_public_ip.worker_publicip.*.ip_address}"
 }
 
 output "admin" {
-    value = "${var.administrator_username}"
+  value = "${var.administrator_username}"
 }
 
 output "ssh" {
-    value = "${var.administrator_ssh}"
+  value = "${var.administrator_ssh}"
 }
 
 output "subscription_id" {
-    value = "${lookup(var.azure_authorization_terraform, "subscription_id")}"
+  value = "${var.azure_authorization_terraform["subscription_id"]}"
 }
 
 output "tenant_id" {
-    value = "${lookup(var.azure_authorization_terraform, "tenant_id")}"
+  value = "${var.azure_authorization_terraform["tenant_id"]}"
 }
 
 output "client_id" {
-    value = "${lookup(var.azure_authorization_terraform, "client_id")}"
+  value = "${var.azure_authorization_terraform["client_id"]}"
 }
 
 output "client_secret" {
-    value = "${lookup(var.azure_authorization_terraform, "client_secret")}"
+  value = "${var.azure_authorization_terraform["client_secret"]}"
 }
 
 output "region" {
-    value = "${azurerm_resource_group.resourcegroup.location}"
+  value = "${azurerm_resource_group.resourcegroup.location}"
 }
 
 output "subnet" {
-    value = "${azurerm_subnet.subnet.name}"
+  value = "${azurerm_subnet.subnet.name}"
 }
 
 output "vnet" {
-    value = "${azurerm_virtual_network.network.name}"
+  value = "${azurerm_virtual_network.network.name}"
 }
